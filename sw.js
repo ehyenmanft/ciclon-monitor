01eb250bde47e4055c219e8133b96953e9dca8a4
James Aranda <ehyenma.nft@gmail.com>
2026-08-08 23:15:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *    viejos como frescos.
  * Al actualizar la web, sube también este archivo cambiando VERSION.
  */
-const VERSION = 'v8';
+const VERSION = 'v9';
 const CACHE = 'ciclon-monitor-' + VERSION;
 
 const PRECACHE = [
